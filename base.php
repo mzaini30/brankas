1ef69afad11bd340daafbe743dd73b4198910608
mzaini30 <muhzaini30@gmail.com>
2020-05-13 07:52:22 +0800

    dari termux

---
+++ base.php
@@ -0,0 +1,6 @@
+<?php
+include "vendor/autoload.php";
+$db = (new \Pecee\Pixie\Connection('sqlite', [
+    'driver'   => 'sqlite',
+    'database' => $_SERVER["DOCUMENT_ROOT"] . 'database.sqlite'
+]))->getQueryBuilder();
